5f9cd83c201d8104998c6b08c303927a3ebc9d0c
Eric <eric@Erics-MacBook-Pro.local>
2019-10-13 22:24:52 -0500

    MVP

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./components/Login";
+import BubblePage from './components/BubblePage';
 import "./styles.scss";
+import PrivateRoute from "./utils/PrivateRoute";
 
 function App() {
   const [colorList, setColorList] = useState([]);
@@ -10,10 +12,7 @@ function App() {
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <PrivateRoute path="/bubble-page" component={BubblePage} />
       </div>
     </Router>
   );
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,56 @@
-import React from "react";
+import React, { useState }  from "react";
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-const Login = () => {
+const Login = props => {
+  const [creds, setCreds] = useState({
+    username: '',
+    password: ''
+})
+
+const changeHandler = e => {
+  setCreds({
+    ...creds,
+    [e.target.name]: e.target.value
+  })
+}
+
+const submitHandler = e => {
+  e.preventDefault();
+  setCreds({ username: '', password: '' })
+
+axiosWithAuth()
+  .post('/login', creds)
+  .then(res => {
+    localStorage.setItem('token', res.data.payload)
+    props.history.push('/bubble-page')
+  })
+  .catch(err => console.log(err))
+}
+
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
-    <>
+    <div>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+      <form onSubmit={submitHandler}>
+        <label htmlFor='username'>Username</label>
+        <input
+          type='text'
+          name='username'
+          value={creds.username}
+          onChange={changeHandler}
+        />
+        <label htmlFor='password'>Password</label>
+        <input
+          type='text'
+          name='password'
+          value={creds.password}
+          onChange={changeHandler}
+        />
+        <button type='submit'>Login</button>
+      </form>   
+    </div>   
+  )
+}
 
 export default Login;
